ecc2316a9f03820521c0b8cb88d8a10675c9bc8b
Tushar12152 <tusharimran470@gmail.com>
2024-01-01 09:36:45 +0600

    navbar added

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,9 +1,28 @@
+import Responsive from "../Utils/Responsive";
+import { FaSearch } from "react-icons/fa";
 
 const Nav = () => {
     return (
-        <div>
-            
+      <Responsive>
+          <div>
+            <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <img className="lg:w-36 w-20" src={'https://i.ibb.co/B3hFDNN/mobile-phone-in-hand-with-information-on-display-line-icon-info-for-customer-on-smartphone-screen-li.jpg'} alt="" />
+
+    <h1 className="font-bold lg:text-4xl bg-clip-text text-transparent bg-gradient-to-r from-purple-500 to-pink-500">
+    Mobile Info
+</h1>
+  </div>
+  <div className="flex-none gap-2">
+    <div className=" flex gap-1">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+      <button className="btn bg-gradient-to-r from-purple-500 to-pink-500 "><FaSearch /></button>
+    </div>
+    
+  </div>
+</div>
         </div>
+      </Responsive>
     );
 };
 
